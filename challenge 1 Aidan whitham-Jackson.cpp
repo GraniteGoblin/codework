8c4b622fab47fa661449f9e33cf686e1c79b7c60
GraniteGoblin <w014692p@student.staffs.ac.uk>
2025-11-24 12:40:59 +0000

    Add files via upload

---
+++ challenge 1 Aidan whitham-Jackson.cpp
@@ -0,0 +1,31 @@
+﻿// Challenge 1 Ascii Art generation of a boat.
+
+#include <iostream>
+using namespace std;
+
+// This code prints the characters needed to make a boat in ascii art.
+
+int main()
+{
+	cout << "                   |\n";
+	cout << "                  /|\\ \n";
+	cout << "                 / | \\     a boat \n";
+	cout << "                /  |  \\ \n";
+	cout << "               / --|-- \\ \n";
+	cout << "              /    |    \\ \n";
+	cout << "             /-----|-----\\ \n";
+	cout << "      |----\\       |        /----|\n";
+	cout << "        \\- - -------------- - -/\n";
+	cout << "         \\ - -- - - - - - -- -/\n";
+	cout << "   /\\  /\\ \\/\\  /\\  /\\  /\\  /\\/ /\\  /\\  \n";
+	cout << "  /  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\  \n";
+	cout << "";
+	cout << "";
+	cout << "";
+	return 0;
+}
+
+
+
+
+
